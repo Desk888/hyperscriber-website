a1b242ee499aa981acda31322b5187c2967a88c4
Desk888 <work.lorenzofilippinI@gmail.com>
2025-05-01 21:55:19 +0200

    Added GTM tag in a different way

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import TagManager from "react-gtm-module"
 
-const tagManagerArgs = {
-    gtmId: "GTM-TVZCBR63"
-}
-TagManager.initialize(tagManagerArgs);
 
 createRoot(document.getElementById("root")!).render(<App />);
