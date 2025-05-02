c96c0ad5faf6658f35863fb0c2a23090bd2e66fd
Desk888 <work.lorenzofilippinI@gmail.com>
2025-05-02 17:06:19 +0200

    Corrected implementations of GTM tag issue of not registering page events.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useEffect } from "react";
 import TopBanner from "./components/layout/TopBanner";
 import Index from "./pages/Index";
 import Services from "./pages/Services";
@@ -16,6 +16,7 @@ import NotFound from "./pages/NotFound";
 import CookieConsent from "./components/layout/CookieConsent";
 import ChatBot from "./components/layout/ChatBot";
 import SecurityHeaders from "./components/security/SecurityHeaders";
+import TagManager from "react-gtm-module";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -46,33 +47,42 @@ const queryClient = new QueryClient({
   },
 });
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner position="top-center" />
-      <BrowserRouter>
-        <SecurityHeaders />
-        <div className="flex flex-col min-h-screen">
-          <TopBanner />
-          <div className="flex-1">
-            <Routes>
-              <Route path="/" element={<Index />} />
-              <Route path="/services" element={<Services />} />
-              <Route path="/pricing" element={<Pricing />} />
-              <Route path="/why-us" element={<WhyUs />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="/terms-conditions" element={<TermsConditions />} />
-              <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
+const App = () => {
+  useEffect(() => {
+    const tagManagerArgs = {
+      gtmId: 'GTM-TVZCBR63',
+    };
+    TagManager.initialize(tagManagerArgs);
+  }, []);
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner position="top-center" />
+        <BrowserRouter>
+          <SecurityHeaders />
+          <div className="flex flex-col min-h-screen">
+            <TopBanner />
+            <div className="flex-1">
+              <Routes>
+                <Route path="/" element={<Index />} />
+                <Route path="/services" element={<Services />} />
+                <Route path="/pricing" element={<Pricing />} />
+                <Route path="/why-us" element={<WhyUs />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/terms-conditions" element={<TermsConditions />} />
+                <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </div>
+            <CookieConsent />
+            <ChatBot />
           </div>
-          <CookieConsent />
-          <ChatBot />
-        </div>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+};
 
 export default App;
